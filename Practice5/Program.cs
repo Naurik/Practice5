d095d7afec9d71f10736b0bde3ccd0644f34fa34
Nauryzbai <nauzik@mail.ru>
2019-04-03 18:07:04 +0600

    Практика5

---
+++ Practice5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool resultOfFirstConversion, resultOfSecondConversion;
+            int beginningOfRange, endOfRange;
+            Random rnd = new Random();
+
+            Console.Write("Введите начало диапазона: ");
+            resultOfFirstConversion = int.TryParse(Console.ReadLine(), out beginningOfRange);
+            Console.Write("Введите конец диапазона: ");
+            resultOfSecondConversion = int.TryParse(Console.ReadLine(), out endOfRange);
+
+            Console.WriteLine();
+
+            if (resultOfFirstConversion && resultOfSecondConversion)
+            {
+                if (beginningOfRange > endOfRange) Swap(ref beginningOfRange, ref endOfRange);
+
+                RangeOfArray arbitraryArray = new RangeOfArray(beginningOfRange, endOfRange);
+
+                for (int i = arbitraryArray.BeginningOfRange; i <= arbitraryArray.EndOfRange; i++)
+                {
+                    arbitraryArray[i] = rnd.Next(10, 100);
+                    Console.WriteLine("Массив[{0}]: {1}", i, arbitraryArray[i]);
+                }
+            }
+        }
+
+        static void Swap(ref int beginningOfRange, ref int endOfRange)
+        {
+            int temp = beginningOfRange;
+            beginningOfRange = endOfRange;
+            endOfRange = temp;
+        }
+    }
+    }
+}
---
+++ Practice5/RangeOfArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice5
+{
+    public class RangeOfArray
+    {
+        private object[] array;
+        public RangeOfArray(int beginningOfRange, int endOfRange)
+        {
+            BeginningOfRange = beginningOfRange;
+            EndOfRange = endOfRange;
+            array = new object[endOfRange - beginningOfRange + 1];
+        }
+        public int BeginningOfRange { get; set; }
+        public int EndOfRange { get; set; }
+
+        public object this[int i]
+        {
+            get
+            {
+                if (BeginningOfRange > 0)
+                    return array[Math.Abs(i) - BeginningOfRange];
+                else
+                    return array[Math.Abs(BeginningOfRange) + i];
+            }
+            set
+            {
+                if (BeginningOfRange > 0)
+                    array[Math.Abs(i) - BeginningOfRange] = value;
+                else
+                    array[Math.Abs(BeginningOfRange) + i] = value;
+            }
+        }
+    }
+}
